414710902bbddf4a83318256f42b864ad9049c51
InkWell Studio <joepro@tuta.io>
2023-02-15 12:36:58 -0600

    restructure content architecture

--- routes/read/[...index].tsx
+++ routes/read/[...index].tsx
@@ -1,27 +1,44 @@
-import Content from '../../islands/content.tsx';
+import { PageProps } from "$fresh/server.ts";
+
+import TableOfContents from '../../islands/table-of-contents.tsx';
+import { ContentContainer } from '../../components/content-container.tsx';
 import { ActionBar } from '../../components/action-bar.tsx';
 
-export default function Home() {
+export default function Home(props: PageProps) {
     return (
         <div class='flex flex-col'>
-            <div class='flex justify-center'>
-                <main class='
-                        relative bg-tan-50 text-justify h-[min-content]
-                        rounded-md shadow md:shadow-2xl
-                        w-full md:max-w-2xl lg:max-w-3xl
-                        px-6 xs:px-10 sm:px-20 lg:px-32
-                        pb-4 pt-4 sm:pt-8 md:pt-14 md:my-8 lg:pt-16
-                    '>
-                    <Content></Content>
-                </main>
+
+            {/*
+            <div>
+                <div class="m-6 p-2">1 {JSON.stringify(props.data)}</div>
+                <div class="m-6 p-2">2 {JSON.stringify(props.params)}</div>
+                <div class="m-6 p-2">3 {props.route}</div>
+                <div class="m-6 p-2">4 {JSON.stringify(props.url)}</div>
+                <div class="m-6 p-2">5 {JSON.stringify(props.url.search)}</div>
+                <div class="m-6 p-2">6 {JSON.stringify(props.url.searchParams)}</div>
+                <div class="m-6 p-2">7 {JSON.stringify(props.url.hash)}</div>
+                <div class="m-6 p-2">8 {JSON.stringify(props.url.host)}</div>
+                <div class="m-6 p-2">9 {JSON.stringify(props.url.hostname)}</div>
+                <div class="m-6 p-2">10 {JSON.stringify(props.url.href)}</div>
+                <div class="m-6 p-2">11 {JSON.stringify(props.url.origin)}</div>
+                <div class="m-6 p-2">12 {JSON.stringify(props.url.password)}</div>
+                <div class="m-6 p-2">13 {JSON.stringify(props.url.pathname)}</div>
+                <div class="m-6 p-2">14 {JSON.stringify(props.url.port)}</div>
+                <div class="m-6 p-2">15 {JSON.stringify(props.url.protocol)}</div>
+                <div class="m-6 p-2">16 {JSON.stringify(props.url.username)}</div>
+                <div class="m-6 p-2">17 {JSON.stringify(props.url.toString)}</div>
+                <div class="m-6 p-2">18 {JSON.stringify(props.url.toJSON)}</div>
             </div>
-            {
-                /*
-                <div>
-                    <ActionBar></ActionBar>
-                </div>
-                */
-            }
+            */}
+
+            {props.params.index}
+
+            <ContentContainer></ContentContainer>
+            {/*
+            <div>
+                <ActionBar></ActionBar>
+            </div>
+            */}
         </div>
     );
 }
