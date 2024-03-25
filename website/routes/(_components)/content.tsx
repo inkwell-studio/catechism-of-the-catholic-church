35aa2c418fa0a6e929f51f8ffb0843c968c04a7b
InkWell Studio <joepro@tuta.io>
2024-03-25 15:43:23 -0500

    formatting

--- website/routes/(_components)/content.tsx
+++ website/routes/(_components)/content.tsx
@@ -7,10 +7,7 @@ import { ContentContainer, Language } from '../../../catechism/source/types/type
 export function Content(props: { content: ContentContainer; language: Language }): JSX.Element {
     return (
         <main class='h-fit'>
-            <div class='
-                    relative text-justify
-                    w-full md:max-w-2xl lg:max-w-3xl
-                    '>
+            <div class='relative text-justify w-full md:max-w-2xl lg:max-w-3xl'>
                 <ContentBase content={props.content} language={props.language}></ContentBase>
             </div>
         </main>
